173b8bf4bc325c2889506cbb297f85a09cb5e338
Clark68500 <mcevoyclark@gmail.com>
2022-11-03 11:05:59 +0100

    updates

--- src/components/BeverageForm.jsx
+++ src/components/BeverageForm.jsx
@@ -3,6 +3,7 @@ import { Navigate } from 'react-router-dom';
 import axios from "axios";
 import { AuthContext } from "../context/auth.context";
 import { MdNoDrinks } from 'react-icons/md';
+import imgPng from '../img/—Pngtree—drink icon_4690686.png'
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
@@ -25,7 +26,6 @@ function BeverageForm({addNewBeverage}) {
         const value = e.target.value;
         const name = e.target.name;
         setNewBeverage({...newBeverage,[name]:value});
-        console.log(newBeverage)
     }
 
     const handleFileUpload = (e) => {
@@ -34,7 +34,6 @@ function BeverageForm({addNewBeverage}) {
         axios
           .post(`${API_URL}/admin/upload`,uploadData)
           .then(response => {
-            console.log("response is: ", response.data);
             setImageUrl(response.data.fileUrl);
             setNewBeverage({...newBeverage,imageUrl:response.data.fileUrl})
           })
@@ -46,47 +45,28 @@ function BeverageForm({addNewBeverage}) {
       }, [])
 
   return (
-    <div className="bg-secondary rounded-3xl text-primary shadow-md mx-10 p-5">
+    <div className="bg-secondary rounded-3xl text-primary shadow-md mx-10 p-5 w-fit mx-auto">
         <form className="m-auto" onSubmit={(e)=>addNewBeverage(e, newBeverage)} >
-            <div className="title mx-20">
+            <div className="title-in-container mx-20">
                 New beverage form
             </div>
     <div className="flex space-x-6 items-center">
         <div className="px-20 flex-wrap">
             <div>
-                <div>
-                        <label className="text-fourthy">Product name</label>
-                        <input className="input" type="text" name="name" onChange={handleChange}></input>
+                <div className="w-96 h-96 bg-ternary overflow-hidden p-5 rounded-2xl my-5">
+                    {!imageUrl ? 
+                    <img src={imgPng} alt="" className="object-contain"/> :
+                    <img src={imageUrl} alt="" className="object-contain"/>}
                 </div>
+            <div className="flex space-x-10">
                 <div>
-                        <label className="text-fourthy">Price</label>
-                        <input className="input" type="number" name="price" onChange={handleChange}></input>
-                </div>
-                <div>
-                        <label className="text-fourthy">Quantity</label>
-                        <input className="input" type="number" name="quantity" onChange={handleChange}></input>
-                </div>
-            </div>
-            </div>
-            <div>
-                <label className="text-fourthy text-center">Choose a type of drink</label>
-                <div className="flex flex-col justify-evenly">
-                    <button className='form-button-choose' name="type" value="Soft" onClick={handleChange}>Soft drink</button>
-                    <button className='form-button-choose' name="type" value="Liquor" onClick={handleChange}>Liquor & Spirit</button>
-                    <button className='form-button-choose' name="type" value="Undistilled" onClick={handleChange}>Undistilled drink</button>
-                </div>
-            </div>
-            <div>
-                <MdNoDrinks size={"300px"} className="bg-ternary rounded-lg p-10"/>
-                <div>
-                    <div>
-                    <label className=" text-fourthy">Image upload</label>
+                    <label className="text-fourthy">Choose a type of drink</label>
+                    <div className="flex flex-col justify-evenly">
+                        <button className='form-button-choose' name="type" value="Soft" onClick={handleChange}>Soft drink</button>
+                        <button className='form-button-choose' name="type" value="Liquor" onClick={handleChange}>Liquor & Spirit</button>
+                        <button className='form-button-choose' name="type" value="Undistilled" onClick={handleChange}>Undistilled drink</button>
                     </div>
-                    <input className="w-60" type="file" name="imageUrl" onChange={(e)=>{handleFileUpload(e)}}></input>
                 </div>
-            </div>
-        </div>
-            <div>
             {newBeverage.type === "Soft" && 
                         <div>
                             <div>
@@ -128,8 +108,31 @@ function BeverageForm({addNewBeverage}) {
                                 </div>
             }
         </div>
-        <div className="text-center">
-            <button className="form-button" type="submit">Upload</button>
+            <div>
+                <div>
+                <label className=" text-fourthy">Image upload</label>
+                </div>
+                <input className="w-full" type="file" name="imageUrl" onChange={(e)=>{handleFileUpload(e)}}></input>
+            </div>
+        </div>
+            <div>
+                <div>
+                        <label className="text-fourthy">Product name</label>
+                        <input className="input" type="text" name="name" onChange={handleChange}></input>
+                </div>
+                <div>
+                        <label className="text-fourthy">Price</label>
+                        <input className="input" type="number" name="price" onChange={handleChange}></input>
+                </div>
+                <div>
+                        <label className="text-fourthy">Quantity</label>
+                        <input className="input" type="number" name="quantity" onChange={handleChange}></input>
+                </div>
+            </div>
+            <div className="">
+            <button className="form-button w-full" type="submit">Upload</button>
+        </div>
+        </div>
         </div>
         </form>
     </div>
